62c3e76bfbb94f1e02e810fde4aade8f163324eb
Morgan Jackson <contactmojack@gmail.com>
2015-08-17 13:59:29 -0500

    Buttons added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Create List app in which you can add and remove items
+// 1. Add 'input' destination {use val()}
+// 2. Prepend 'input' to current list push new-item 
+// 3. Check off 'input'
+// 3. Remove 'input'
+// 4. Reset entire list.
+
+$(document).ready(function() {
+
+});
