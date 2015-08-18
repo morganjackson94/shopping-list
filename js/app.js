63f80ee00356a9ed4192b1c11289d2b26e48de56
Morgan Jackson <contactmojack@gmail.com>
2015-08-18 13:04:15 -0500

    js test

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
-// Create List app in which you can add and remove items
-// 1. Add 'input' destination {use val()}
-// 2. Prepend 'input' to current list push new-item 
-// 3. Check off 'input'
-// 3. Remove 'input'
-// 4. Reset entire list.
-
 $(document).ready(function() {
-
+	$('#item').val();
+	console.log();
 });
