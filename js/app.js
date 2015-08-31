41dabeba468d9e000b923162c70fb9e036856201
Morgan Jackson <contactmojack@gmail.com>
2015-08-31 13:40:07 -0500

    js debug 2

--- js/app.js
+++ js/app.js
@@ -4,20 +4,34 @@ function addItem() {
 	var list = $('.list-container');
 	var item = $('<li><button class="fa fa-minus-square-o"></button><span class="new-item">' + write + '</span><button class="fa fa-check-square-o"></button></li>');
 	var newItem = $('.list');
+	var placeholder = $('.placehold')
 
 	if (write.length === 0) {
 		return false;
 	}
 
+	placeholder.remove();
 	list.append(item);
 	$(newItem).val('');
 
 }
 
+function deleteItem() {
+	$(newItem).remove();
+}
+
+
 $(function() {
 	var newItem = $('.list');
+	var minus = $('<button class="fa fa-minus-square-o">');
+	var list = $('.list-container');
 
-	newItem.on('keypress', function(e) {
+
+//user removes list item
+	list.on('click', minus, deleteItem);
+
+//user adds list item by pressing enter
+	newItem.on('keypress', function (e) {
         if (e.which == 13) {
             addItem();
         }
@@ -25,6 +39,8 @@ $(function() {
 });
 
 
-	//user adds list item by pressing enter
-	//user removes list item
+
+
+	
+	
 	//user completes list item
