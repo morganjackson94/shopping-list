d8a1fb2e2a62c874d51bf7de05ac59b510cf4862
Morgan Jackson <contactmojack@gmail.com>
2015-08-31 15:53:46 -0500

    js debug 3

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //user inputs list item
+var newItem = $('.item');
+
 function addItem() {
-	var write = $('.item').val();
-	var list = $('.list-container');
+	console.log("add item");
+	var list = $('.list');
 	var item = $('<li><button class="fa fa-minus-square-o"></button><span class="new-item">' + write + '</span><button class="fa fa-check-square-o"></button></li>');
-	var newItem = $('.list');
-	var placeholder = $('.placehold')
+	var placeholder = $('.placehold');
 
 	if (write.length === 0) {
 		return false;
@@ -17,29 +18,29 @@ function addItem() {
 }
 
 function deleteItem() {
-	$(newItem).remove();
+	$(this).remove();
 }
 
 
 $(function() {
-	var newItem = $('.list');
+	var newItem = $('.item');
 	var minus = $('<button class="fa fa-minus-square-o">');
-	var list = $('.list-container');
+	var list = $('.list');
 
 
 //user removes list item
 	list.on('click', minus, deleteItem);
 
 //user adds list item by pressing enter
-	newItem.on('keypress', function (e) {
-        if (e.which == 13) {
-            addItem();
-        }
-	});
+	newItem.on('submit', function(e) {
+		var write = $('.item').val();
+    	e.preventDefault();
+       	addItem();
+    })
 });
 
 
-
+// jquery submit 
 
 	
 	
