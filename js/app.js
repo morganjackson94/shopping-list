df9936b711330c0ae95d09aa648d2e52f68d082f
Morgan Jackson <contactmojack@gmail.com>
2015-08-31 13:14:03 -0500

    js debugging

--- js/app.js
+++ js/app.js
@@ -2,15 +2,8 @@
 function addItem() {
 	var write = $('.item').val();
 	var list = $('.list-container');
-	var item = $('<li>
-					<span></span>
-					<button class="remove">
-						<img src="http://www.clker.com/cliparts/0/7/e/a/12074327311562940906milker_X_icon.svg.hi.png" width="20px">
-					</button>' + write + '<button class="check">
-						<img src="http://www.clker.com/cliparts/u/e/E/I/2/i/dark-green-check-mark-hi.png" alt="check" width="25px">
-					</button>
-				</li>');
-	var newItem = $('.list')
+	var item = $('<li><button class="fa fa-minus-square-o"></button><span class="new-item">' + write + '</span><button class="fa fa-check-square-o"></button></li>');
+	var newItem = $('.list');
 
 	if (write.length === 0) {
 		return false;
@@ -22,11 +15,15 @@ function addItem() {
 }
 
 $(function() {
-	newItem.on('keypress', function (e) {
+	var newItem = $('.list');
+
+	newItem.on('keypress', function(e) {
         if (e.which == 13) {
-            addListItem();
+            addItem();
         }
-}
+	});
+});
+
 
 	//user adds list item by pressing enter
 	//user removes list item
